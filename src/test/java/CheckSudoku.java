1ce4d32364ce42072f717255b1c597190ee7a32e
isaacribeiro <ribeiro.isaac@hotmail.com>
2018-07-23 22:24:14 -0300

    Missing Tests

--- src/test/java/CheckSudoku.java
+++ src/test/java/CheckSudoku.java
@@ -1,10 +1,55 @@
 import org.junit.Test;
+import org.junit.Assert;
 
 public class CheckSudoku {
 
     @Test
+    public void validSingleRowOrColumn(){
+
+//      Input structure
+        int[] actRecord = new int[5, 3, 4, 6, 7, 8, 9, 1, 2];
+
+        assertTrue(Sudoku.checkSingleRecord(actRecord));
+    }
+
+    @Test
+    public void invalidSingleRowOrColumn(){
+
+//      Input structure
+        int[] actRecord = new int[5, 3, 4, 5, 7, 8, 9, 1, 2];
+
+        assertFalse(Sudoku.checkSingleRecord(actRecord));
+
+    }
+
+    @Test
+    public void validSingleBlock(){
+
+//      Expected Object
+        Sudoky actBlock = new Sudoku( [ 5, 3, 4],
+                                      [ 6, 7, 2],
+                                      [ 1, 9, 8]);
+
+        assertTrue(Sudoku.checkSingleBlock(actBlock));
+
+    }
+
+    @Test
+    public void invalidSingleBlock(){
+
+//      Expected Object
+        Sudoky expSudoku = new Sudoku( [ 5, 3, 4],
+                                       [ 6, 5, 2],
+                                       [ 1, 9, 8]);
+
+        assertFalse(Sudoku.checkSingleBlock(actBlock));
+
+    }
+
+    @Test
     public void checkCalculatedSudoku(){
 
+//      Input Object
         Sudoku actSudoku = new Sudoku( [ 5, 3,  ,  , 7,  ,  ,  ,  ],
                                        [ 6,  ,  , 1, 9, 5,  ,  ,  ],
                                        [  , 9, 8,  ,  ,  ,  , 6,  ],
@@ -15,6 +60,8 @@ public class CheckSudoku {
                                        [  ,  ,  , 4, 1, 9,  ,  , 5],
                                        [  ,  ,  ,  , 8,  ,  , 7, 9] );
 
+
+//       Expected Object
         Sudoky expSudoku = new Sudoku( [ 5, 3, 4, 6, 7, 8, 9, 1, 2],
                                        [ 6, 7, 2, 1, 9, 5, 3, 4, 8],
                                        [ 1, 9, 8, 3, 4, 2, 5, 6, 7],
@@ -25,6 +72,7 @@ public class CheckSudoku {
                                        [ 2, 8, 7, 4, 1, 9, 6, 3, 5],
                                        [ 3, 4, 5, 2, 8, 6, 1, 7, 9] );
 
+        assertEquals(expSudoku, calculateSudoku(actSudoku));
 
     }
 }
