c49295cedf174db248cba5663495010a1f6d5691
isaacribeiro <ribeiro.isaac@hotmail.com>
2018-07-25 12:06:20 -0300

    Bug Fixing

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sudoku {
 
     private int[][] elements = new int[9][9];
@@ -8,12 +10,12 @@ public class Sudoku {
 
 //  Check if a single record fits the base Sudoku condition of having no zeros, repeated values or numbers out of 1..9 interval
     public static boolean checkSingleRecord(int[] record){
-        return false;
+        return hasNonRepeatedValues(record);
     }
 
 //  Check if a single block fits the base Sudoku condition of having no zeros, repeated values or number out of 1..9 interval
     public static boolean checkSingleBlock(int[][] block){
-        return false;
+        return hasNonRepeatedValues(serializesMultimensionalArray(block));
     }
 
 //  Receives a incomplete Sudoku and tries to solve it
@@ -37,6 +39,59 @@ public class Sudoku {
 
         return result;
 
+    }
+
+//  Check if there is no repeated values for a specific set
+    private static boolean hasNonRepeatedValues(int[] entries){
+
+//      Boxed list for Wrapper's Auxiliary Methods Support
+        Integer[] boxedList = Arrays.stream( entries )
+                .boxed()
+                .toArray(Integer[]::new);
+
+        List<Integer> aList = Arrays.asList(boxedList);
+
+//      A set doesn't allow non-unique values
+        Set<Integer> adjustedSet = new HashSet<Integer>();
+
+        aList.stream()
+                .filter(x -> x >= 1 && x <= 9)
+                .filter(x -> !adjustedSet.contains(x))
+                .forEach(adjustedSet::add);
+
+        return adjustedSet.size() == 9 ? true : false;
+
+    }
+
+//  Serializes an multidimensional Array
+    private static int[] serializesMultimensionalArray(int[][] input){
+
+//      Generic Model for Multidimensional Array Serialization Process
+//      int[] to Integer[] -> Boxing Process
+        Integer[] boxedList;
+
+//      Creates an ArrayList<Integer> to support multidimensional appending process
+        List<Integer> aList = new ArrayList<Integer>();
+
+//      Gets the content of each line and append to the Integer Boxed List
+        for(int aRow = 0; aRow < input.length; aRow++) {
+
+//          Cast the int[] row for a Boxed format
+            boxedList = Arrays.stream(input[aRow])
+                    .boxed()
+                    .toArray(Integer[]::new);
+            aList.addAll(Arrays.asList(boxedList));
+
+        }
+
+//      Auxiliary swapping process.
+//      Before casting into an int[], creates an Integer[]
+        Integer[] aReturn = aList.toArray(new Integer[aList.size()]);
+
+//      Cast to primitive int[] type
+        return Arrays.stream(aReturn)
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
 }
